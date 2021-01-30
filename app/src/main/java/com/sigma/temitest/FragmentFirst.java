ad5156778fd1feb8e10990816d0e93c053b813be
sun010421 <sun010421>
2021-01-30 18:12:09 +0900

    First Commit

---
+++ app/src/main/java/com/sigma/temitest/FragmentFirst.java
@@ -0,0 +1,49 @@
+package com.sigma.temitest;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class FragmentFirst extends Fragment {
+
+    private MainActivity mainActivity = (MainActivity)getActivity();
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        if(context instanceof MainActivity){
+            mainActivity = (MainActivity) context;
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(
+                R.layout.fragment_1p, container, false);
+        return rootView;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        final Button homepage = (Button) getView().findViewById(R.id.homepage_btn);
+        homepage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), Web.class));
+            }
+        });
+
+    }
+
+}
